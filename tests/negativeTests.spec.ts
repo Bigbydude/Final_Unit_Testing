ef01b6403f095c88615fb00577efe64f57faedac
Panko Alexander <panko.alexander94@gmail.com>
2022-08-17 19:24:14 +0300

    Fix

--- tests/negativeTests.spec.ts
+++ tests/negativeTests.spec.ts
@@ -5,30 +5,30 @@ import { VALUES_NEGATIVE, VALUES_POSITIVE } from '../Helpers/valuesForCheck';
 
 describe('Negative test', () => {
     it('Check login', () => {
-        expect(Registration.loginField(V_NEGATIVE.LOGIN_FIRST)).to.equal(VALUES_NEGATIVE.LOGIN_FIRST);
+        expect(Registration.loginField(V_NEGATIVE.LOGIN_FIRST)).not.to.equal(VALUES_NEGATIVE.LOGIN_FIRST);
     });
 
     it('Check login2', () => {
-        expect(Registration.loginField(V_NEGATIVE.LOGIN_SECOND)).to.equal(VALUES_POSITIVE.LOGIN_SECOND);
+        expect(Registration.loginField(V_NEGATIVE.LOGIN_SECOND)).not.to.equal(VALUES_POSITIVE.LOGIN_SECOND);
     });
 
     it('Check login3', () => {
-        expect(Registration.loginField(V_NEGATIVE.LOGIN_THIRD)).to.equal(VALUES_POSITIVE.LOGIN_THIRD);
+        expect(Registration.loginField(V_NEGATIVE.LOGIN_THIRD)).not.to.equal(VALUES_POSITIVE.LOGIN_THIRD);
     });
 
     it('Check password', () => {
-        expect(Registration.passwordField(V_NEGATIVE.PASSWORD_FIRST)).to.equal(VALUES_NEGATIVE.PASSWORD_FIRST);
+        expect(Registration.passwordField(V_NEGATIVE.PASSWORD_FIRST)).not.to.equal(VALUES_NEGATIVE.PASSWORD_FIRST);
     });
 
     it('Check password2', () => {
-        expect(Registration.passwordField(V_NEGATIVE.PASSWORD_SECOND)).to.equal(VALUES_POSITIVE.PASSWORD_SECOND);
+        expect(Registration.passwordField(V_NEGATIVE.PASSWORD_SECOND)).not.to.equal(VALUES_POSITIVE.PASSWORD_SECOND);
     });
 
     it('Check email', () => {
-        expect(Registration.mailField(V_NEGATIVE.EMAIL_FIRST)).to.equal(VALUES_NEGATIVE.EMAIL_FIRST);
+        expect(Registration.mailField(V_NEGATIVE.EMAIL_FIRST)).not.to.equal(VALUES_NEGATIVE.EMAIL_FIRST);
     });
 
     it('Check email2', () => {
-        expect(Registration.mailField(V_NEGATIVE.EMAIL_SECOND)).to.equal(VALUES_POSITIVE.EMAIL_SECOND);
+        expect(Registration.mailField(V_NEGATIVE.EMAIL_SECOND)).not.to.equal(VALUES_POSITIVE.EMAIL_SECOND);
     });
 });
